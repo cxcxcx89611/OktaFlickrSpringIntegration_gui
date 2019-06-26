de572a8058e951bbdf986f303741ca7479ed5dcf
Chen <xichen@ciena.com>
2019-06-26 23:37:27 +1000

    prod

--- src/components/islogin.vue
+++ src/components/islogin.vue
@@ -3,14 +3,14 @@
     <div style="text-align: center; padding: 10px; border-bottom: 1px solid gainsboro">
       <span>please input keyword </span>
       <el-input style="width: 200px;" v-model="name"/>
-      <el-button @click="search">search</el-button>
+      <el-button @click="search">search </el-button>
     </div>
     <div>
       <div v-if="data.length > 0">
         <img v-for="item in data" :src="item" style=" width: 200px; height: 200px; padding: 10px; "/>
       </div>
       <div v-else style="margin: 0 auto; padding: 40px; text-align: center">
-         No more content
+        no more content....
       </div>
     </div>
   </div>
@@ -29,7 +29,7 @@
     methods: {
       search() {
         const reqServer = this.$axios.create({
-          baseURL: "https://35.188.162.0:8443", // api 的 base_url
+          baseURL: "https://35.188.162.0:8443", // api  base_url
           timeout: 100000,
         })
         const s = reqServer({
@@ -37,8 +37,8 @@
           method: 'get',
         })
         s.then(res => {
-          console.log(res.data)
-          this.data = res.data;
+          console.log(res.data.data )
+          this.data = res.data.data;
         })
         console.log(s)
       }
