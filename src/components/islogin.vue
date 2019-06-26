b8ac04caa2019d24ede17940d869ae3acae0ac30
Chen <xichen@ciena.com>
2019-06-26 22:40:41 +1000

    iterate json payload photo

--- src/components/islogin.vue
+++ src/components/islogin.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div style="text-align: center; padding: 10px; border-bottom: 1px solid gainsboro">
-      <span>搜输入搜索关键词: </span>
+      <span>please input keyword </span>
       <el-input style="width: 200px;" v-model="name"/>
-      <el-button @click="search">搜索</el-button>
+      <el-button @click="search">search</el-button>
     </div>
     <div>
       <div v-if="data.length > 0">
         <img v-for="item in data" :src="item" style=" width: 200px; height: 200px; padding: 10px; "/>
       </div>
       <div v-else style="margin: 0 auto; padding: 40px; text-align: center">
-        暂无更多内容
+        no more content to be displayed
       </div>
     </div>
   </div>
@@ -29,7 +29,7 @@
     methods: {
       search() {
         const reqServer = this.$axios.create({
-          baseURL: "https://35.188.162.0:8443", // api 的 base_url
+          baseURL: "https://localhost:8443", // api 的 base_url
           timeout: 100000,
         })
         const s = reqServer({
@@ -37,8 +37,8 @@
           method: 'get',
         })
         s.then(res => {
-          console.log(res.data)
-          this.data = res.data;
+          console.log(res.data.data)
+          this.data = res.data.data;
         })
         console.log(s)
       }
